5047b7464c345934b811b31e0f0fb4a51d832f83
pirbhai14 <pirbhai14@hotmail.com>
2022-12-13 15:24:04 +0400

    Update Icon Weather Option

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import plotly.express as px
+import backend
+from PIL import Image
+
+
+def get_img(weather):
+    img = Image.open(f"Images/{weather}")
+    st.image(img, width=200)
 
 
 st.title('Weather Forecast  for the next days')
@@ -12,3 +20,34 @@ option = st.selectbox('Select data to view', options=['Temperature', 'Sky'])
 msg = f"Temperature for the next {days} days in {place}"
 
 st.subheader(msg)
+
+
+if place and option == 'Temperature':
+    try:
+        dates, temperatures = backend.get_data(place=place,forecast_days= days, kind=option)
+        figure = px.line(x=dates,y=temperatures, labels={'x': 'Dates', 'y': 'Temperatures' })
+        st.plotly_chart(figure)
+    except TypeError:
+        st.text('Place name is incorrect.')
+    except NameError:
+        st.text('Place name is incorrect.')
+
+
+
+elif place and option == 'Sky':
+    try:
+        dates, weather = backend.get_data(place=place,forecast_days= days, kind=option)
+        images = {'Clouds': 'Images/cloud.png', 'Clear': 'Images/clear.png', 'Snow': 'Images/snow.png', 'Rain': 'Images/rain.png'}
+        img_paths = [images[condition] for condition in weather] 
+        print(img_paths) 
+        st.image(img_paths, width=110, caption= dates)
+    except TypeError:
+        st.text('Place name is incorrect.')
+    except TypeError:
+        st.text('Place name is incorrect.')
+    
+    
+
+
+
+  
